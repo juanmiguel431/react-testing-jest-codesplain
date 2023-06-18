b15f2c13ef73882fcfebd649c305a41041c2b154
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-18 11:47:12 -0400

    RepositoriesListItem test added.

---
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,40 @@
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import RepositoriesListItem from './RepositoriesListItem';
+
+const MockComponent = () => {
+  return <h1>Mocked Component</h1>;
+};
+
+jest.mock('../tree/FileIcon.js', () => {
+  return () => <MockComponent />;
+});
+
+test('Shows a link to the github homepage for this repository', async () => {
+  const repositoryMock = {
+    html_url: 'https://github.com/facebook/react',
+    full_name: 'facebook/react', language: 'Javascript', description: 'A Js library', owner: 'Facebook', name: 'react',
+  };
+
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repositoryMock}/>
+    </MemoryRouter>
+  );
+
+  // await screen.findByRole('img', { name: 'Javascript' });
+
+  // screen.debug();
+  // await pause();
+  // screen.debug();
+
+  // const link = screen.getByText(new RegExp(repositoryMock.html_url, 'i'));
+  //
+  // expect(link).toBeInTheDocument();
+});
+
+const pause = () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 100);
+  });
+};
