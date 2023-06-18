9ccf96917b60d635d84a891ae66ce6a6b96abc26
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-18 12:20:27 -0400

    RepositoriesListItem test - Shows a FileIcon with the appropriate icon - test added.

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,7 +2,7 @@ import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import RepositoriesListItem from './RepositoriesListItem';
 
-test('Shows a link to the github homepage for this repository', async () => {
+function renderComponent() {
   const repositoryMock = {
     html_url: 'https://github.com/facebook/react',
     full_name: 'facebook/react', language: 'Javascript', description: 'A Js library', owner: 'Facebook', name: 'react',
@@ -14,8 +14,21 @@ test('Shows a link to the github homepage for this repository', async () => {
     </MemoryRouter>
   );
 
+  return { repositoryMock };
+}
+
+test('Shows a link to the github homepage for this repository', async () => {
+  const { repositoryMock } = renderComponent();
+
   await screen.findByRole('img', { name: 'Javascript' });
 
   const link = screen.getByRole('link', { name: /github repository/i });
   expect(link).toHaveProperty('href', repositoryMock.html_url);
+});
+
+test('Shows a FileIcon with the appropriate icon', async () => {
+  const { repositoryMock } = renderComponent();
+  const icon = await screen.findByRole('img', { name: 'Javascript' });
+
+  expect(icon).toHaveClass('js-icon');
 });
