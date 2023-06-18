cfc726e20cf459de5516d308090670a15b452229
juanmiguel431 <juanmiguel431@gmail.com>
2023-06-18 19:06:27 -0400

    HomeRoute test added - Mocking http request

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -7,21 +7,19 @@ function HomeRoute() {
   const { data: tsRepos } = useRepositories('stars:>10000 language:typescript');
   const { data: rustRepos } = useRepositories('stars:>10000 language:rust');
   const { data: goRepos } = useRepositories('stars:>10000 language:go');
+  const { data: pythonRepos } = useRepositories('stars:>10000 language:python');
+  const { data: javaRepos } = useRepositories('stars:>10000 language:java');
 
   return (
     <div>
       <Hero />
       <div className="container mx-auto py-8 grid grid-cols-1 gap-4 md:grid-cols-2">
-        <RepositoriesTable
-          label="Most Popular Javascript"
-          repositories={jsRepos}
-        />
-        <RepositoriesTable
-          label="Most Popular Typescript"
-          repositories={tsRepos}
-        />
+        <RepositoriesTable label="Most Popular Javascript" repositories={jsRepos}/>
+        <RepositoriesTable label="Most Popular Typescript" repositories={tsRepos}/>
         <RepositoriesTable label="Most Popular Rust" repositories={rustRepos} />
         <RepositoriesTable label="Most Popular Go" repositories={goRepos} />
+        <RepositoriesTable label="Most Popular Python" repositories={pythonRepos} />
+        <RepositoriesTable label="Most Popular Java" repositories={javaRepos} />
       </div>
     </div>
   );
